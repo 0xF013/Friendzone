7712065c737053a938c168730ee04c78ff1669c9
Mihail Maxacov <0xf013@gmail.com>
2013-04-19 15:03:50 +0300

    first blood

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendzone::Application.config.secret_token = 'f867ad5389b1c4db3a1fa29d625f68e26804b53729804514f5804571dab1c6963387fb1ff290cd342286e2cbbcea75efc8c08c07f704cab38f9eec2d94c09d31'
